e645e65b69de2c0374ede1dd094b01377e004b4f
Luis Saavedra <luisdavidsag@gmail.com>
2026-01-14 21:24:15 -0500

    Add terraform files
    Add terraform files for ecr and pipelines CI/CD para terraform
    plan, lint and deploy

---
+++ terraform/outputs.tf
@@ -0,0 +1,15 @@
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.api.repository_url
+}
+
+output "ecr_repository_name" {
+  description = "Name of the ECR repository"
+  value       = aws_ecr_repository.api.name
+}
+
+output "ecr_repository_arn" {
+  description = "ARN of the ECR repository"
+  value       = aws_ecr_repository.api.arn
+}
+
