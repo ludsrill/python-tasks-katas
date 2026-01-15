cc657e198c322479132a96e997632bc9d2ee3b61
Luis Saavedra <luisdavidsag@gmail.com>
2026-01-14 22:29:36 -0500

    Final adjusment

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,6 @@ terraform {
     }
   }
 
-  # Backend S3 - Descomenta después de crear el bucket y DynamoDB
-  # Ejecuta primero: terraform apply (sin backend)
-  # Luego descomenta esto y ejecuta: terraform init -migrate-state
   backend "s3" {
     bucket         = "codewars-katas-terraform-state-343218193902"
     key            = "codewars-katas/terraform.tfstate"
